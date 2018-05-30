04a2bbbd3ce046c2cc9afcb751a28e94c174acfa
sunqkd <sunzhaoqkd@163.com>
2018-05-30 20:30:47 +0800

    'up'

--- src/app/starts/starts.component.ts
+++ src/app/starts/starts.component.ts
@@ -6,15 +6,15 @@ import { Component, Input, OnInit } from '@angular/core';
 	styleUrls: ['./starts.component.css']
 })
 export class StartsComponent implements OnInit {
-    @Input() // 说明rating是从外部接受的 默认为0
-	rating:number = 0;
+	@Input() // 说明rating是从外部接受的 默认为0
+	rating: number = 0;
 
-	starts:boolean [];
+	starts: boolean[];
 	constructor() { }
 
 	ngOnInit() {
 		this.starts = [] // 初始化
-		for(let i = 1;i<=5;i++){
+		for (let i = 1; i <= 5; i++) {
 			this.starts.push(i > this.rating)
 		}
 	}
